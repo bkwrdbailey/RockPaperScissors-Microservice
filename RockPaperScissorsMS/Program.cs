6c9c2129907601c2f5b7806903775c1a563333a6
bkwrdbailey <Baileybarton77@yahoo.com>
2022-11-27 13:22:37 -0600

    Changed url for backend calls

--- RockPaperScissorsMS/Program.cs
+++ RockPaperScissorsMS/Program.cs
@@ -18,7 +18,7 @@ builder.Services.AddCors(options =>
                         });
 });
 
-builder.Services.Configure<UrlConfiguration>(builder.Configuration.GetSection("UrlConfigurations"));
+builder.Services.Configure<UrlConfiguration>(builder.Configuration.GetSection("UrlConfiguration"));
 
 builder.Services.AddControllers().AddJsonOptions(options => options.JsonSerializerOptions.PropertyNamingPolicy = null);
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
